50aa3648cbfda20fcf1d68a1ca75ef03681e0796
alice <ajsc1@aluno.ifal.edu.brm>
2023-07-25 00:30:41 -0300

    db

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const port = 3000;
 const app = express();
 
 app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://127.0.0.1:27017/tododb");
+
+const taskSchema = { name: String };
+
+const Task = mongoose.model("Task", taskSchema);
+
+const task1 = new Task({
+  name: "Welcome to the nyan to do list.",
+});
+
+const task2 = new Task({
+  name: "<--- Hit this to delete a item.",
+});
+
+const task3 = new Task({
+  name: "Hit the + to add a item.",
+});
+
+const defaultTasks = [task1, task2, task3];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -39,38 +60,61 @@ let month = months[d.getMonth()];
 
 let day = days[d.getDay()];
 
-let item = [" ", " ", " "];
 let i = 0;
 
 function addList(task) {
-  if (i <= 2) {
-    if (item[i] == " ") {
-      item[i] = task;
-    }
-    i++;
-  } else {
-    item.push(task);
-  }
+  const newTask = new Task({ name: task });
+
+  defaultTasks.push(task);
+  newTask.save();
+  console.log(defaultTasks);
 }
 
-const data = {
-  items: item,
-  day: day,
-  month: month,
-  date: d.getDate(),
-};
-app.get("/", (req, res) => {
-  res.render("index.ejs", data);
-  console.log(data.items);
+app.get("/", async (req, res) => {
+  await Task.find({}).then(function (findItems) {
+    if (findItems.length === 0) {
+      Task.insertMany(defaultTasks)
+        .then(function () {
+          console.log("Data inserted"); // Success
+        })
+        .catch(function (error) {
+          console.log(error); // Failure
+        });
+      res.redirect("/");
+    } else {
+      res.render("index.ejs", {
+        content: findItems,
+        day: day,
+        month: month,
+        date: d.getDate(),
+      });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
-  const task = req.body["newItem"];
+  const task = req.body.newItem;
+
   addList(task);
 
   res.redirect("/");
 });
 
+app.post("/delete", function (req, res) {
+  const delTask = req.body.checkbox;
+  console.log("Task ID select: ", delTask);
+
+  Task.findByIdAndRemove(delTask).then(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("task excluded!");
+    }
+  });
+
+  res.redirect("/");
+});
+
 app.listen(port, function () {
-  console.log("server ir running on", +port);
+  console.log("server is running on", +port);
 });
